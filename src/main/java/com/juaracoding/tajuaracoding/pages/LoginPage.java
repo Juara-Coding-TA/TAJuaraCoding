07ef3123f89fca6926186bef3719b77bba35d724
Adhitya Ardhiansyah <104175819+Adhitya2808@users.noreply.github.com>
2025-08-14 23:59:55 +0700

    Add Test: Logout Test

--- src/main/java/com/juaracoding/tajuaracoding/pages/LoginPage.java
+++ src/main/java/com/juaracoding/tajuaracoding/pages/LoginPage.java
@@ -9,6 +9,8 @@ public class LoginPage {
     public By emailField = By.id("email");
     public By passwordField = By.id("password");
     public By loginButton = By.xpath("//*[@id=\"__next\"]/div/div/div[2]/div/div[2]/form/button");
+    public By logoutButton = By.xpath("//*[@id=\"profile-menu\"]/div[3]/ul/div/button[2]");
+    public By miniButton = By.xpath("//*[@id=\"__next\"]/div/header/div/button[3]");
     public By errorMessageAccountNotFound = By.xpath("//*[@id=\"__next\"]/div/div/div[2]/div/div[1]/div/div/div/div[2]/p");
     public By errorMessageEmailorPassword = By.xpath("//*[@id=\"__next\"]/div/div/div[2]/div/div[1]/div/div/div/div[2]/p");
 
@@ -31,6 +33,14 @@ public class LoginPage {
 
     public void clickButton() {
         driver.findElement(loginButton).click();
+    }
+
+    public void miniButton(){
+        driver.findElement(miniButton).click();
+    }
+
+    public void logoutButton(){
+        driver.findElement(logoutButton).click();
     }
 
     public String getCurrentUrl() {
--- src/test/java/com/juaracoding/tajuaracoding/Login/PositiveLogin.java
+++ src/test/java/com/juaracoding/tajuaracoding/Login/PositiveLogin.java
@@ -1,5 +1,6 @@
 package com.juaracoding.tajuaracoding.Login;
 
+import org.testng.Assert;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
@@ -27,5 +28,32 @@ public class PositiveLogin extends BaseTest{
         System.out.println("Current URL after login: " + currentUrl);
         
         // Optionally, you can add assertions here to validate the successful login
-    }    
+    }
+
+    @Test
+    @Parameters({"username", "password"})
+    public void Logout(String username, String password) throws InterruptedException {
+        // Initialize the LoginPage with the current driver
+        LoginPage loginPage = new LoginPage(DriverUtil.getDriver());
+
+        // Open the login page
+        loginPage.openLoginPage();
+
+        // Perform login
+        loginPage.login(username, password);
+        Thread.sleep(1000);
+        // Verify successful login by checking the URL or any other element that indicates success
+        String currentUrl = loginPage.getCurrentUrl();
+        System.out.println("Current URL after login: " + currentUrl);
+
+        // Clicking Button For Logout
+        loginPage.miniButton();
+        Thread.sleep(100);
+        loginPage.logoutButton();
+
+        // Verify successful logout by checking the URL
+        String currentUrls = loginPage.getCurrentUrl();
+        System.out.println("Current URL: " + currentUrls);
+        Assert.assertEquals(currentUrls,"https://magang.dikahadir.com/authentication/login");
+    }
 }
