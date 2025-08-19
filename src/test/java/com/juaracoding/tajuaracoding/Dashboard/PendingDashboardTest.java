915c6b5b9a703f50897c2b7b896119940b07cc50
Adhitya Ardhiansyah <104175819+Adhitya2808@users.noreply.github.com>
2025-08-19 12:43:50 +0700

    add: Test initialPage pending dashboard, filterDate
    jangan dimerge dulu ini adalah backup

---
+++ src/test/java/com/juaracoding/tajuaracoding/Dashboard/PendingDashboardTest.java
@@ -0,0 +1,119 @@
+package com.juaracoding.tajuaracoding.Dashboard;
+
+import com.juaracoding.tajuaracoding.BaseTest;
+import com.juaracoding.tajuaracoding.pages.DashBoardPage;
+import com.juaracoding.tajuaracoding.utils.DriverUtil;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class PendingDashboardTest extends BaseTest {
+    DashBoardPage dashBoardPage;
+
+    @Test(enabled = true)
+    public void testVerifyPage() throws InterruptedException {
+        dashBoardPage = new DashBoardPage(DriverUtil.getDriver());
+        Thread.sleep(2000);
+        Assert.assertTrue(dashBoardPage.getCurrentUrl().contains("pending"),"URL halaman tidak sesuai!");
+        Assert.assertTrue(DriverUtil.getDriver().findElement(dashBoardPage.searchButton).isDisplayed(),"Tombol Search tidak ditemukan!");
+        Assert.assertTrue(DriverUtil.getDriver().findElement(dashBoardPage.resetButton).isDisplayed(),"Tombol Reset tidak ditemukan!");
+        Assert.assertTrue(DriverUtil.getDriver().findElement(dashBoardPage.filterByUnitButton).isDisplayed(),"Icon filter merah tidak ditemukan!");
+    }
+
+    @Test()
+    public void DateRangeFilter() throws InterruptedException {
+        dashBoardPage = new DashBoardPage(DriverUtil.getDriver());
+        Thread.sleep(2000);
+        dashBoardPage.clickIconCalendar();
+        dashBoardPage.selectDateRange(0,18);
+        String startDate = dashBoardPage.getStartDateValue();
+        String endDate = dashBoardPage.getEndDateValue();
+
+        Assert.assertTrue(startDate.contains("01-08-2025"),"Start Date tidak sesuai");
+        Assert.assertTrue(endDate.contains("19-08-2025"), "End Date tidak sesuai");
+
+        Thread.sleep(2000);
+        dashBoardPage.clickSaveCalendar();
+        dashBoardPage.clickSearch();
+        Thread.sleep(2000);
+
+        //------Validator1-------
+        //Lembur
+        if (dashBoardPage.validator1Lembur.isEmpty()) {
+            System.out.println("Validator1 - Lembur kosong.");
+            Assert.assertEquals(dashBoardPage.validator1Lembur.size(), 0);
+        } else {
+            System.out.println("Validator1 - Lembur ada data.");
+            Assert.assertTrue(dashBoardPage.validator1Lembur.size() > 0);
+        }
+        //Cuti
+        if (dashBoardPage.validator1Cuti.isEmpty()) {
+            System.out.println("Validator1 - Cuti kosong.");
+            Assert.assertEquals(dashBoardPage.validator1Cuti.size(), 0);
+        } else {
+            System.out.println("Validator1 - Cuti ada data.");
+            Assert.assertTrue(dashBoardPage.validator1Cuti.size() > 0);
+        }
+        //Koreksi
+        if (dashBoardPage.validator1Koreksi.isEmpty()) {
+            System.out.println("Validator1 - Koreksi kosong.");
+            Assert.assertEquals(dashBoardPage.validator1Koreksi.size(), 0);
+        } else {
+            System.out.println("Validator1 - Koreksi ada data.");
+            Assert.assertTrue(dashBoardPage.validator1Koreksi.size() > 0);
+        }
+        //------Validator2-------
+        if (dashBoardPage.validator2Lembur.isEmpty()) {
+            System.out.println("Validator2 - Lembur kosong.");
+            Assert.assertEquals(dashBoardPage.validator2Lembur.size(), 0);
+        } else {
+            System.out.println("Validator2 - Lembur ada data.");
+            Assert.assertTrue(dashBoardPage.validator2Lembur.size() > 0);
+        }
+        //Cuti
+        if (dashBoardPage.validator2Cuti.isEmpty()) {
+            System.out.println("Validator2 - Cuti kosong.");
+            Assert.assertEquals(dashBoardPage.validator2Cuti.size(), 0);
+        } else {
+            System.out.println("Validator2 - Cuti ada data.");
+            Assert.assertTrue(dashBoardPage.validator2Cuti.size() > 0);
+        }
+        //Koreksi
+        if (dashBoardPage.validator2Koreksi.isEmpty()) {
+            System.out.println("Validator2 - Koreksi kosong.");
+            Assert.assertEquals(dashBoardPage.validator2Koreksi.size(), 0);
+        } else {
+            System.out.println("Validator2 - Koreksi ada data.");
+            Assert.assertTrue(dashBoardPage.validator2Koreksi.size() > 0);
+        }
+
+        if (dashBoardPage.validator3Lembur.isEmpty()) {
+            System.out.println("Validator3 - Lembur kosong.");
+            Assert.assertEquals(dashBoardPage.validator3Lembur.size(), 0);
+        } else {
+            System.out.println("Validator3 - Lembur ada data.");
+            Assert.assertTrue(dashBoardPage.validator3Lembur.size() > 0);
+        }
+        //Cuti
+        if (dashBoardPage.validator3Cuti.isEmpty()) {
+            System.out.println("Validator3 - Cuti kosong.");
+            Assert.assertEquals(dashBoardPage.validator3Cuti.size(), 0);
+        } else {
+            System.out.println("Validator3 - Cuti ada data.");
+            Assert.assertTrue(dashBoardPage.validator3Cuti.size() > 0);
+        }
+        //Koreksi
+        if (dashBoardPage.validator3Koreksi.isEmpty()) {
+            System.out.println("Validator3 - Koreksi kosong.");
+            Assert.assertEquals(dashBoardPage.validator3Koreksi.size(), 0);
+        } else {
+            System.out.println("Validator3 - Koreksi ada data.");
+            Assert.assertTrue(dashBoardPage.validator3Koreksi.size() > 0);
+        }
+    }
+
+    @Test
+    public void positivefilterTest(){
+
+    }
+
+}
